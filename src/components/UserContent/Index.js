322c47b86557dda993d019780af16b9568653e75
Vterebenin <niceserver11@mail.ru>
2019-08-03 14:10:20 +0300

    connect redux to the user content

--- src/components/UserContent/Index.js
+++ src/components/UserContent/Index.js
@@ -1,59 +1,82 @@
 import { Card } from 'antd'
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { userData } from '../mocks/realdata';
+import Loader from '../Loader/Index'
 import imgLvl1 from './files/lvl1.jpg'
 import imgLvl2 from './files/lvl2.jpg'
 import imgLvl3 from './files/lvl3.jpg'
+import {
+    fetchAreaIfNeeded,
+} from '../../actions'
 const { Meta } = Card;
 
-export default class UserContent extends Component {
+class UserContent extends Component {
     constructor(props) {
         super(props)
         this.Data = userData;
-        // console.log("Data", this.Data)
+        const { dispatch } = this.props
+        dispatch(fetchAreaIfNeeded())
     }
-    
-    
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.props.user !== prevProps.user) {
+            const { dispatch } = this.props
+            dispatch(fetchAreaIfNeeded())
+        }
+    }
+
     render() {
         let img;
-        const {grade} = this.Data;
+        const { user } = this.props
+        console.log(user, "here");
         // console.log(grade);
-        switch (grade.toLowerCase()) {
-            case "ученик":
-                img = imgLvl2        
-                break;
-            case "мастер":
-                img = imgLvl3        
-                break;
-            default:
-                img = imgLvl1        
-                break;
-        }
+        if (user) {
+            const { grade } = user
+            switch (grade.toLowerCase()) {
+                case "ученик":
+                    img = imgLvl2
+                    break;
+                case "мастер":
+                    img = imgLvl3
+                    break;
+                default:
+                    img = imgLvl1
+                    break;
+            }
+            const Desc = () => {
+                const { name, grade, date_of_employment } = user
 
-        const Desc = () => {
-            const { name, grade, date_of_employment } = this.Data
-            
-            
-            return ( 
-                <React.Fragment>
-                    <h2>Хэй, {name}!</h2> 
-                    <h1>Да ты у нас: {grade}</h1>
-                    <h3>И работяга аж с {date_of_employment}</h3>
-                </React.Fragment>
+                return (
+                    <React.Fragment>
+                        <h2>Хэй, {name}!</h2>
+                        <h1>Да ты у нас: {grade}</h1>
+                        <h3>И работяга аж с {date_of_employment}</h3>
+                    </React.Fragment>
+                )
+            }
+
+            return (
+                <Card
+                    hoverable
+                    style={{ width: "100%" }}
+                    cover={<img alt="example" src={img} />}
+                >
+                    <Meta description={<Desc />} />
+                </Card>
             )
         }
 
-        return (
-            <Card
-                hoverable
-                style={{ width: "100%" }}
-                
-                cover={<img alt="example" src={img} />}
-
-            >
-                {/* cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />} */}
-                <Meta description={<Desc />} />
-            </Card>
-        )
+        return <Loader />
     }
 }
+
+function mapStateToProps(state) {
+    const { lastUpdated, user } = state.userData
+    return {
+        lastUpdated,
+        user
+    }
+}
+
+export default connect(mapStateToProps)(UserContent) 
