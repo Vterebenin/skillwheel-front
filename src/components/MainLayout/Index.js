ab9facbf19d38db1b9e0cad84892b668afae8dcc
VaTerebenin <te@web-canape.com>
2019-08-26 16:10:37 +0300

    refractoring

--- src/components/MainLayout/Index.js
+++ src/components/MainLayout/Index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Breadcrumb, Row, Col, Button } from 'antd';
+import { Breadcrumb, Button } from 'antd';
 import Wheel from '../Wheel/Index'
 import UserContent from '../UserContent/Index';
 import SkillContent from '../SkillContent/Index'
@@ -47,40 +47,43 @@ class SiderDemo extends React.Component {
 		const { user } = this.props
 		return (
 			<MainWrapper>
-				<Row gutter={100} className="w-contentWrapper">
-					<Breadcrumb style={{ margin: '16px 0' }}>
-						<Breadcrumb.Item>Мой профиль</Breadcrumb.Item>
-						{user && <Breadcrumb.Item>{user.name}</Breadcrumb.Item>}
-					</Breadcrumb>
-					<h1>Ваш профиль:</h1>
-					<Col span={8}>
-						{this.props.skillId
-							? (
-								<Button onClick={this.onButtonClick}>
-									{this.state.content ? 'Посмотреть текущий скилл' : 'Назад в профиль'}
-								</Button>
-							)
-							: (
-								<Button disabled>
-									{this.state.content ? 'Посмотреть текущий скилл' : 'Назад в профиль'}
-								</Button>
-							)
-						}
+				<div className="main">
+					<div className="main__pathline">
+						<Breadcrumb style={{ margin: '16px 0' }}>
+							<Breadcrumb.Item>Мой профиль</Breadcrumb.Item>
+							{user && <Breadcrumb.Item>{user.name}</Breadcrumb.Item>}
+						</Breadcrumb>
+					</div>
+					<div className="main__content">
+						<div className="main__col main__info">
+							<h1>Ваш профиль:</h1>
+							{this.props.skillId
+								? (
+									<Button onClick={this.onButtonClick}>
+										{this.state.content ? 'Посмотреть текущий скилл' : 'Назад в профиль'}
+									</Button>
+								)
+								: (
+									<Button disabled>
+										{this.state.content ? 'Посмотреть текущий скилл' : 'Назад в профиль'}
+									</Button>
+								)
+							}
 
-						{this.state.content ? (
-							<UserContent />
-						) : (
-								<SkillContent />
-							)}
-
-					</Col>
-					<Col span={16}>
-						<div ref={(divElement) => this.divElement = divElement}>
+							{this.state.content ? (
+								<UserContent />
+							) : (
+									<SkillContent />
+								)}
+						</div>
+						<div className="main__col main__wheel" ref={(divElement) => this.divElement = divElement}>
 							<Wheel clickHandler={this.skillClick} />
 						</div>
-					</Col>
-				</Row>
-			</MainWrapper>
+					</div>
+				</div>
+
+
+			</MainWrapper >
 		);
 	}
 }
